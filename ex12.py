e5bec2a82b4481597e49ce182c8e9c74c7e64e2e
Antonio Carmona <carmonantonio@gmail.com>
2016-12-27 23:17:51 -0400

    La de hoy

---
+++ ex12.py
@@ -0,0 +1,4 @@
+age = input("How old are you? ")
+height = input ("How tall are you? ")
+weight = input ("how much do you weight? ")
+print ("So you are %r old, %r tall and %r heavy" %(age,height,weight))
---
+++ ex5.py
@@ -0,0 +1,19 @@
+# This is the exercise but with my own data and with the metrc system.
+
+name = "Antonio Carmona"
+age = 34
+height = 1.70
+weight = 110
+eyes = "Brown"
+teeth = "White"
+hair = "Black"
+
+print ("Let's talk about %s" % name)
+print ("He's %d Mts tall" % height)
+print ("He's %d Kg. heavy" % weight)
+print ("Actually that's not too heavy.")
+print ("He's got %s eyes and %s hair." % (eyes, hair))
+print ("His teeth are usually %s depending on the coffee." % teeth)
+
+
+print ("If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight))
