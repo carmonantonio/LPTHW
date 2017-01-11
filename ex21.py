67b733c216490ca723ee273ff6a8bcdb78b37dcd
Antonio Carmona <carmonantonio@gmail.com>
2017-01-11 19:57:15 -0400

    another one

--- ex21.py
+++ ex21.py
@@ -7,4 +7,18 @@ def subtract(a,b):
     return a-b
 
 def multiply(a,b):
-    p
+    print ("Multiplying %d * %d" % (a,b))
+    return a * b
+
+def divide (a,b):
+    print ("DIVIDING %d / %d" % (a,b))
+    return a / b
+
+print ("Let´s do some math wwith just functions!")
+
+age = add(30,5)
+height = subtract(78,4)
+weight = multiply(90,2)
+iq = divide(100,2)
+
+print ("Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq))
